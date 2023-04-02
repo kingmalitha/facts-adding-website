41c5bbec951ca53e0f209eb5e88edfabdd067b20
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2023-04-02 16:19:18 +0530

    4. Dividing Our Interface Into Components

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -2,14 +2,35 @@ import "./style.css";
 
 function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" height="68" width="68" alt="main-logo" />
-        <h1>Today I Learned!</h1>
-      </div>
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+    <>
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" height="68" width="68" alt="main-logo" />
+          <h1>Today I Learned!</h1>
+        </div>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+
+      <NewFactForm />
+
+      <main className="main">
+        <CategoryFilters />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
+function CategoryFilters() {
+  return <aside>CategoryFilter</aside>;
+}
+
+function FactList() {
+  return <section>Facts list</section>;
 }
 
 export default App;
