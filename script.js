58b44ab761d5cb19bad35ff20880a30186a095f1
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2023-04-02 10:32:37 +0530

    11. Looping Over Arrays The forEach and map Methods

--- script.js
+++ script.js
@@ -55,3 +55,28 @@ console.log(factObj["text"]);
 const { category, isCorrect } = factObj;
 console.log(category);
 console.log(factObj.createSummary());
+
+//For each
+[2, 4, 6, 8].forEach(function (el) {
+  console.log(el);
+});
+
+//Map
+const times10 = [2, 4, 6, 8].map(function (el) {
+  return el * 10;
+});
+console.log(times10);
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const allCategories = CATEGORIES.map((el) => el.name);
+console.log(allCategories);
