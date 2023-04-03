fed87ac0c6b603e520ade305c5d91123b0c5a0d5
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2023-04-03 07:34:36 +0530

    15. Creating a Loader Component

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -13,49 +13,27 @@ const CATEGORIES = [
   { name: "news", color: "#8b5cf6" },
 ];
 
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
-
 function App() {
   //  1. define state variable
   const [showForm, setShowForm] = useState(false);
   const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
     async function getFacts() {
-      let { data: facts, error } = await supabase.from("facts").select("*");
-      setFacts(facts);
+      setIsLoading(true);
+      let { data: facts, error } = await supabase
+        .from("facts")
+        .select("*")
+        .order("votesInteresting", { ascending: false })
+        .limit(1000);
+
+      if (!error) {
+        setFacts(facts);
+        setIsLoading(false);
+      } else {
+        alert("there was a problem getting data");
+      }
     }
     getFacts();
   }, []);
@@ -70,11 +48,20 @@ function App() {
 
       <main className="main">
         <CategoryFilters />
-        <FactList facts={facts} setFacts={setFacts} />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <FactList facts={facts} setFacts={setFacts} />
+        )}
       </main>
     </>
   );
 }
+
+function Loader() {
+  return <p className="message">Loading ...</p>;
+}
+
 function Header({ showForm, setShowForm }) {
   const appTitle = "Today I Learned";
   return (
