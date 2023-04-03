4003df512e877e6bd1e8d63e3d953853971288f6
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2023-04-03 14:17:38 +0530

    1. Finishing Touches and Creating a Production Build

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -230,6 +230,8 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(columnName) {
     setIsUpdating(true);
@@ -250,6 +252,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
+        {isDisputed ? <span className="disputed">[🚫DISPUTED]</span> : null}
         {fact.text}
         <a className="source" href={fact.source}>
           (Source)
